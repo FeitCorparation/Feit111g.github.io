6ec93ba17790785cc3fb12f02be743770c51288e
FeitCorparation <getsdaw71@gmail.com>
2026-01-08 01:22:21 -1200

    Add TypeScript interfaces for ServiceItem, Achievement, and Benefit

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface ServiceItem {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export interface Achievement {
+  value: string;
+  label: string;
+}
+
+export interface Benefit {
+  title: string;
+  text: string;
+}
